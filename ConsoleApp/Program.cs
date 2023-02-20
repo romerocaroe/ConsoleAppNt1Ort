4c2c66279e0cdd4e38ecce2207d7f41795266324
xcxr12 <CRomero2@pan-energy.com>
2023-02-20 09:34:00 -0300

    Hello World!

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; //Esto es como el import de java
+                //aquí le estoy diciendo que use una liubrería base del gramework que se llama System
+
+namespace PrimerEjemplo //Espacio de nombres, relacionado con la compilación y visibilidad de las librerias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Espera leer algo tipeado desde la consola
+            Console.ReadKey();
+        }
+    }
+}
